6fa2b67876bba80385d0c0eb23c8093e7119be7e
Manny Sayah <mannys@dataismore.com>
2021-05-26 22:03:19 +1200

    02:40:00 Event loop

---
+++ 16-js-fallback.js
@@ -0,0 +1,9 @@
+setInterval(
+    () => {
+        console.log('hello asgard');
+    },
+    2000);
+console.log(`I will run first`);
+// process stays alive unless
+// Kill Process CTRL+C
+// or unexpected error
--- app.js
+++ app.js
@@ -1,22 +1,14 @@
-// NPM
+const http = require('http');
 
-// Install locally
-// $ npm i <packageName>
+const server = http.createServer(
+    (req, res) => {
+        console.log('request event');
+        res.end('hello asgard');
+    }
+);
 
-// Install globally
-// $ npm install -g <packageName>
-
-// Three options for creating 'package.json'
-
-// 1- create 'package.json' manually in root and add propereits
-// 2- $ npm init        follow the step by step process | Enter to skip
-// 3- $ npm init -y     accept all default
-
-// testing out the lodash module
-// make sure you installed it before running this file
-// npm i lodash
-
-const _ = require('lodash');
-const items = [1, [2, [3, [4]]]];
-const newItems = _.flattenDeep(items);
-console.log(newItems);
+server.listen(5000,
+    () => {
+        console.log('Server listening on port 5000...');
+    }
+);
